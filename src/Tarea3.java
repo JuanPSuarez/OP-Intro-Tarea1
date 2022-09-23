e02a8146568de012902e22f2244c6c11d03c3d45
JuanSuarez <jp.juansuarez@gmail.com>
2022-09-23 17:31:27 -0300

    Added final version

--- src/Tarea3.java
+++ src/Tarea3.java
@@ -1,9 +1,45 @@
-class Persona{
-    string nombre;
-    int edad;
-    String telefono;
+//Punto 1.
+class Persona {
+    //Punto 2.
+    private String nombre;
+    private int edad;
+    private String telefono;
 
+    //Punto 3.
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+    public String getTelefono(){
+        return this.telefono;
+    }
 }
 public class Tarea3 {
+    //Punto 5
+    public static void main (String[] args){
+        //Punto 4
+        Persona persona1 = new Persona();
+        persona1.setNombre("Juan");
+        persona1.setEdad(25);
+        persona1.setTelefono("+5499999999");
+
+        System.out.print(persona1.getNombre()+ "\n");
+        System.out.print(persona1.getEdad()+ "\n");
+        System.out.print(persona1.getTelefono()+ "\n");
+
+    }
 
 }
